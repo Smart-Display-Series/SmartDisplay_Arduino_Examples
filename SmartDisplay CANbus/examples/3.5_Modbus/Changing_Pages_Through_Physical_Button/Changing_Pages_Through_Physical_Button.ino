897c61d447811e003d614383345c7a1b3a024028
raman <raman@winstar.com.tw>
2023-04-25 08:58:49 +0800

    Modbus Brightness control program

--- SmartDisplay CANbus/examples/3.5_Modbus/Changing_Pages_Through_Physical_Button/Changing_Pages_Through_Physical_Button.ino
+++ SmartDisplay CANbus/examples/3.5_Modbus/Changing_Pages_Through_Physical_Button/Changing_Pages_Through_Physical_Button.ino
@@ -1,85 +1,37 @@
 #include <ModbusMaster.h>
 
-// Modbus parameters
-#define MODBUS_SLAVE_ID 1     // Modbus slave ID of the smart display 
-#define MODBUS_BAUD_RATE 115200 // Baud rate for Modbus communication
-#define MODBUS_PARITY 'N'
-#define MODBUS_DATA_FORMAT 8
-#define MODBUS_SERIAL Serial1 // Modbus slave ID of the smart display
-#define MODBUS_REGISTER_PAGE 0x01 // Modbus register to control the display page
+#define SLAVE_ADDRESS 0x7B
+#define BRIGHTNESS_REGISTER_ADDRESS 2111
+// #define MODBUS_REGISTER_PAGE 2112  // Modbus register to control the display page
 
-// Button configuration
-#define BUTTON_PIN 2  // Pin for the physical button
+#define MIN_BRIGHTNESS 0
+#define MAX_BRIGHTNESS 255
 
-// Page colors
-#define PAGE1_COLOR "red"
-#define PAGE2_COLOR "blue"
+// #define PAGE1_COLOR "red"
+// #define PAGE2_COLOR "blue"
+// const int led = 13;
 
-// Modbus object
-ModbusMaster modbus;
-
-// Button state variables
-bool buttonState = false;
-bool lastButtonState = false; // Initialize lastButtonState with a default value
+ModbusMaster node;
 
 void setup() {
-  // Initialize Serial for Modbus communication
-  MODBUS_SERIAL.begin(static_cast<unsigned long>(MODBUS_BAUD_RATE), MODBUS_DATA_FORMAT);
- 
-  // Initialize button pin as input
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
   
-  // Connect to Modbus device
-  modbus.begin(MODBUS_SLAVE_ID, MODBUS_SERIAL);
+  Serial.begin(115200);
+  node.begin(SLAVE_ADDRESS, Serial); // using the Serial object for communication
+  // node.writeSingleRegister(0x0002, 0x7B06); // Set backlight to 123
 }
 
-
-// void loop() {
-//   // Read button status
-//   int buttonState = digitalRead(BUTTON_PIN);
-  
-//   // Send Modbus command to switch pages based on button status
-//   if (buttonState == LOW) {
-//     // Button pressed, send Modbus command to switch to second page
-//     modbus.writeSingleRegister(0, 1); // Write 1 to register 0 to switch to second page
-//   } else {
-//     // Button released, send Modbus command to switch to first page
-//     modbus.writeSingleRegister(0, 0); // Write 0 to register 0 to switch to first page
-//   }
-
-//   // Wait for a short duration before checking button status again
-//   delay(100);
-// }
-
-
 void loop() {
-  // Read the button state
-  buttonState = digitalRead(BUTTON_PIN);
+  // digitalWrite(led, 1 - digitalRead(led));
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED on by making the voltage Low
+  node.writeSingleRegister(BRIGHTNESS_REGISTER_ADDRESS, MIN_BRIGHTNESS  ); //Minimum brightness level
+  // node.writeSingleRegister(0x2101, 150);
+  delay(1000);
 
-  // Detect button press (falling edge)
-  if (!buttonState && lastButtonState) {
-    // Switch page on the smart display
-    modbus.writeSingleRegister(static_cast<uint16_t>(MODBUS_SLAVE_ID), static_cast<uint16_t>(MODBUS_REGISTER_PAGE));
-
-      Serial.println("Switching to Page 2...");
-      delay(100); // Delay for Modbus communication
-    } else {
-      Serial.println("Failed to switch page!");
-    }
-  
-  // Detect button release (rising edge)
-  if (buttonState && !lastButtonState) {
-    // Switch page on the smart display
-    modbus.writeSingleRegister(static_cast<uint16_t>(MODBUS_SLAVE_ID), static_cast<uint16_t>(MODBUS_REGISTER_PAGE));
-
-      Serial.println("Switching to Page 1...");
-      delay(100); // Delay for Modbus communication
-    } else {
-      Serial.println("Failed to switch page!");
-    }
-  }
-
-  // lastButtonState = buttonState; // Update last button state
-// }
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED off by making the voltage High
+  node.writeSingleRegister(BRIGHTNESS_REGISTER_ADDRESS, MAX_BRIGHTNESS); //Maximum brightness level
+  // node.writeSingleRegister(0x2101, 255);
+  delay(1000);
+}
 
 
