b8266fa559b1ad8274460dad4184ea5ade7421d8
raman <raman@winstar.com.tw>
2023-04-26 13:26:18 +0800

    Switching the page while pressing the physical button.

--- SmartDisplay CANbus/examples/3.5_Modbus/Changing_Pages_Through_Physical_Button/Changing_Pages_Through_Physical_Button.ino
+++ SmartDisplay CANbus/examples/3.5_Modbus/Changing_Pages_Through_Physical_Button/Changing_Pages_Through_Physical_Button.ino
@@ -2,36 +2,87 @@
 
 #define SLAVE_ADDRESS 0x7B
 #define BRIGHTNESS_REGISTER_ADDRESS 2111
-// #define MODBUS_REGISTER_PAGE 2112  // Modbus register to control the display page
+#define MODBUS_REGISTER_PAGE 2112        // Modbus register to control the display page
+#define MODBUS_REGISTER_MODE 2502
+
+#define CONFIG_MODE     0
+#define DISPLAY_MODE    1
+#define BUTTON_PIN      A0           // Change this to the pin number of your physical button
+#define THRESHOLD       500          // Change this to adjust the sensitivity of the physical button unit: (mV)
+// #define DEBOUNCE_TIME   0         // Change this to adjust the debounce time (in ms)
 
 #define MIN_BRIGHTNESS 0
 #define MAX_BRIGHTNESS 255
 
 // #define PAGE1_COLOR "red"
 // #define PAGE2_COLOR "blue"
-// const int led = 13;
+const int led = 13;
+uint16_t buttonValue = 0;
 
 ModbusMaster node;
 
+bool buttonState = HIGH;
+bool lastButtonState = LOW;
+
+unsigned long lastDebounce = 0;
+unsigned long debounceDelay = 60;
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  
+  // pinMode(BUTTON_PIN, INPUT_PULLUP);    // Set the physical button pin as input with pull-up resistor
+  pinMode(BUTTON_PIN, INPUT);
   Serial.begin(115200);
   node.begin(SLAVE_ADDRESS, Serial); // using the Serial object for communication
-  // node.writeSingleRegister(0x0002, 0x7B06); // Set backlight to 123
+}
+
+void chagePageFunction( uint8_t pageValue ) 
+{  
+  // Step 1. Send the device mode command - switch to config mode
+  node.writeSingleRegister(MODBUS_REGISTER_MODE, CONFIG_MODE);
+
+ // Step 5. Send the change page command
+  node.writeSingleRegister(MODBUS_REGISTER_PAGE, pageValue);
+
+  // Step 6. Send the device mode command - switch to display mode
+  node.writeSingleRegister(MODBUS_REGISTER_MODE, DISPLAY_MODE);
 }
 
 void loop() {
-  // digitalWrite(led, 1 - digitalRead(led));
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED on by making the voltage Low
-  node.writeSingleRegister(BRIGHTNESS_REGISTER_ADDRESS, MIN_BRIGHTNESS  ); //Minimum brightness level
-  // node.writeSingleRegister(0x2101, 150);
-  delay(1000);
+  
+  // read physical button voltage
+  int buttonValue = analogRead(BUTTON_PIN);
 
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED off by making the voltage High
-  node.writeSingleRegister(BRIGHTNESS_REGISTER_ADDRESS, MAX_BRIGHTNESS); //Maximum brightness level
-  // node.writeSingleRegister(0x2101, 255);
-  delay(1000);
+  if( buttonValue > THRESHOLD ) {
+    
+    buttonState = HIGH;
+  }
+  else {
+    
+    buttonState = LOW;
+  }
+
+  // if ( buttonState != lastButtonState) {
+
+  //   lastDebounce = millis();        // Starting lastDebounce timing
+  // }
+ 
+  if ( (millis() - lastDebounce) > debounceDelay) {        // Check if delay has passed
+
+    if (lastButtonState != buttonState) {
+
+      lastButtonState = buttonState;
+      
+      if( buttonState == HIGH ) {
+
+        chagePageFunction(1);
+        digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on by making the voltage High
+      }
+      else {
+        
+        chagePageFunction(0);
+        digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage Low
+      }
+    }
+  } 
 }
-
 
